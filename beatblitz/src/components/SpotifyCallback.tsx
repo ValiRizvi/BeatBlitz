fd119491874f0a3b88754b7f32a93d3f5ba07fe9
Vali <valirizvi@gmail.com>
2025-02-23 18:58:34 -0500

    initial commit

--- beatblitz/src/components/SpotifyCallback.tsx
+++ beatblitz/src/components/SpotifyCallback.tsx
@@ -14,10 +14,9 @@ const SpotifyCallback = () => {
         const fixedHash = hash.replace('#', '?');  // query parameters should start with ? so replace hashtag
 
         const params = new URLSearchParams(fixedHash);
-        const token = params.get('access_token')?.trim();;
-        
-        console.log(token)
-        console.log('Token type:', typeof token);
+        const token = params.get('access_token')?.trim();
+        console.log(token);
+
         if (token) {
             localStorage.setItem('spotify_token', token);  // localStorage stores data in the users browser 
             navigate('/');  // redirect to home page
@@ -26,7 +25,7 @@ const SpotifyCallback = () => {
         };
     }, []);
 
-    return <h1>Logging in...</h1>;
+    return <h1>logging in...</h1>;
 };
 
 export default SpotifyCallback;
